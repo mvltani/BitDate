e39a2e95824879ea5822162da07899dfc58a37b5
Patrick Multani <Patrick@patricks-mbp.speedport_w_723v_1_36_000>
2015-04-13 11:45:31 +0200

    Initial Commit

---
+++ BitDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitDate
+//
+//  Created by Patrick Multani on 13/04/15.
+//  Copyright (c) 2015 Less and Better. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
